9e0126acb11ab0e78288e54037d67709568d12e3
Rex Han <rexhanh123@gmail.com>
2025-07-18 12:44:53 -0400

    new footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,28 +1,49 @@
-import { MDBFooter, MDBIcon } from "mdb-react-ui-kit";
+import { MDBFooter, MDBContainer, MDBIcon, MDBBtn } from "mdb-react-ui-kit";
+
 export default function Footer() {
   return (
     <>
-      <div className="fixed bottom-0 left-0 w-full h-16 text-white text-center leading-[4rem]">
-        <MDBFooter className="text-center text-lg-start text-muted bottom-0 w-full">
-          <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-            <div className="me-5 d-none d-lg-block"></div>
-            <div>
-              <a
-                href="https://www.linkedin.com/in/rexhanh/"
-                className="me-4 text-reset"
-              >
-                <MDBIcon color="secondary" fab icon="linkedin" />
-              </a>
-              <a href="https://github.com/rexhanh" className="me-4 text-reset">
-                <MDBIcon color="secondary" fab icon="github" />
-              </a>
-              <a href="mailto:rex.han@berkeley.edu" className="me-4 text-reset">
-                <MDBIcon far icon="envelope" />
-              </a>
-            </div>
+      <MDBFooter className="w-full text-center">
+        <MDBContainer>
+          <section>
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="https://www.linkedin.com/in/rexhanh/!"
+              role="button"
+            >
+              <MDBIcon fab className="fa-linkedin" />
+            </MDBBtn>
+
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="https://github.com/rexhanh"
+              role="button"
+            >
+              <MDBIcon fab className="fa-github" />
+            </MDBBtn>
+
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="mailto:rex.han@berkeley.edu"
+              role="button"
+            >
+              <MDBIcon far icon="envelope" />
+            </MDBBtn>
           </section>
-        </MDBFooter>
-      </div>
+        </MDBContainer>
+      </MDBFooter>
     </>
   );
 }
